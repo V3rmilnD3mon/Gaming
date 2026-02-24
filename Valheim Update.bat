cae04ed08b1a1f3ac28fd180e4ca08326f1be8a0
V3rmilnD3mon <vkvang.jobs@gmail.com>
2026-02-24 12:31:43 -0600

    Delete Valheim Update.bat

--- Valheim Update.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd "C:\Users\D3mon\Desktop\steamcmd"
-
-steamcmd.exe +force_install_dir "C:\Users\D3mon\Desktop\steamcmd\steamapps\common\Valheim dedicated server" +login anonymous +app_update 896660 -beta none validate +quit
