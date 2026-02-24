78967dba12fdbb145e78ca18e2bf2e16fd80249d
V3rmilnD3mon <vkvang.jobs@gmail.com>
2026-02-24 13:00:12 -0600

    Add files via upload

---
+++ Valheim/Valheim Update.bat
@@ -0,0 +1,5 @@
+@echo off
+
+cd "C:\Users\D3mon\Desktop\steamcmd"
+
+steamcmd.exe +force_install_dir "C:\Users\D3mon\Desktop\steamcmd\steamapps\common\Valheim dedicated server" +login anonymous +app_update 896660 -beta none validate +quit
