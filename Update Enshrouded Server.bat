07af581dd31518c2e1beb8a64b0393bb67182b1c
V3rmilnD3mon <vkvang.jobs@gmail.com>
2026-02-24 12:32:22 -0600

    Delete Update Enshrouded Server.bat

--- Update Enshrouded Server.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cd "C:\Users\D3mon\Desktop\steamcmd"
-
-steamcmd +login anonymous +force_install_dir "C:\Users\D3mon\Desktop\steamcmd\steamapps\common\Enshrouded_server" +app_update 2278520 validate +quit
